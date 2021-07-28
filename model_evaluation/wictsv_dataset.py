b6530a081dbe029da9f45e26bb5f0fa8f9912a4e
Artem Revenko <artreven@gmail.com>
2021-07-28 19:59:10 +0200

    Support reading dataset with character offsets (not tokens)

--- model_evaluation/wictsv_dataset.py
+++ model_evaluation/wictsv_dataset.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 from enum import Enum
+from typing import Tuple, List
 
 import torch
 from transformers import PreTrainedTokenizer
@@ -7,6 +8,24 @@ from transformers import PreTrainedTokenizer
 
 class WiCTSVDatasetEncodingOptions(Enum):
     CTX_DEF = '[CLS] context [SEP] definition; hypernyms [SEP]'
+
+
+def char_offset2token_offset(
+        cxt: str,
+        si: int,
+        ei: int) -> Tuple[List[str], int, int]:
+    cxt_ = cxt[:si] + ' MATCH ' + cxt[ei:]
+    target_toks = cxt[si:ei].split()
+    tokens = cxt_.split()
+    for i, w in enumerate(tokens):
+        if w == 'MATCH':
+            tgt_si = i
+            break
+    else:
+        raise IndexError(f'No MATCH found in {tokens}')
+    tgt_ei = tgt_si + len(target_toks)
+    tokens[tgt_si:tgt_si + 1] = target_toks
+    return tokens, tgt_si, tgt_ei
 
 
 class WiCTSVDataset(torch.utils.data.Dataset):
@@ -68,7 +87,7 @@ class WiCTSVDataset(torch.utils.data.Dataset):
         :param text: text to be tokenized
         :return: dict of the format { original_index : [bert_indices]}, list original token index for each bert token
         """
-        original_tokens = text.split(' ')
+        original_tokens = text.split()
         index_list = []
         index_map = defaultdict(list)
 
@@ -123,6 +142,77 @@ class WiCTSVDataset(torch.utils.data.Dataset):
             item['labels'] = self.labels[idx]
         return item
 
-
     def __len__(self):
         return self.len
+
+
+class WiCTSVDatasetCharOffsets(WiCTSVDataset):
+    def __init__(self,
+                 contexts: List[str],
+                 target_ses: List[Tuple[int, int]],
+                 hypernyms: List[List[str]],
+                 definitions: List[str],
+                 tokenizer: PreTrainedTokenizer,
+                 labels: List[int] = None,
+                 focus_token=None,
+                 encoding_type=WiCTSVDatasetEncodingOptions.CTX_DEF):
+        self.len = len(contexts)
+        if labels is not None:
+            self.labels = torch.tensor(labels, dtype=torch.float)
+        else:
+            self.labels = None
+        self.tokenizer = tokenizer
+        if focus_token is not None:
+            contexts, target_inds = self.mark_target_in_context(contexts=contexts,
+                                                                target_ses=target_ses,
+                                                                focus_char=focus_token)
+
+        targets = [cxt[tgt_si:tgt_ei] for cxt, (tgt_si, tgt_ei) in zip(contexts, target_ses)]
+        self.tgt_start_len = []
+        self.descr_start_len = []
+        sense_ids_strs = []
+        for cxt, (tgt_si, tgt_ei), def_, hyps, tgt in zip(contexts, target_ses, definitions, hypernyms, targets):
+            cxt_index_map, cxt_index_list = self._get_token_index_map_and_list(cxt, tokenizer)
+            tokens, tgt_word_si, tgt_word_ei = char_offset2token_offset(cxt, tgt_si, tgt_ei)
+            target_start_ind = cxt_index_map[tgt_word_si][0] + len(['[CLS]'])
+            target_len = len(sum([cxt_index_map[i] for i in range(tgt_word_si, tgt_word_ei)], []))
+            # assert len(tokenizer.tokenize(tgt)) == target_len
+            assert tokens[tgt_word_si:tgt_word_ei] == tgt.split()
+            self.tgt_start_len.append((target_start_ind, target_len))
+
+            sense_identifiers_str = def_ + '; ' + ', '.join(hyps)
+            sense_ids_strs.append(sense_identifiers_str)
+            descrs_start_ind = len(cxt_index_list) + len(['[CLS]', '[SEP]'])
+            descrs_len = len(tokenizer.tokenize(sense_identifiers_str))
+            self.descr_start_len.append((descrs_start_ind, descrs_len))
+
+        if encoding_type == WiCTSVDatasetEncodingOptions.CTX_DEF:
+            tokenizer_input = [[context, sense_ids] for context, sense_ids in zip(contexts, sense_ids_strs)]
+        else:
+            raise NotImplementedError
+
+        self.encodings = tokenizer(tokenizer_input, return_tensors='pt', truncation=True, padding=True)
+
+    @staticmethod
+    def mark_target_in_context(contexts: List[str], target_ses: List[Tuple[int, int]], focus_char: str = "$"):
+        """
+        This method will mark the target word in a context with a special character before and after it,
+        e.g. "This is the target in this sentence" --> "This is the $ target $ in this sentence"
+        :param contexts: list of context strings
+        :param target_inds: list of target indices
+        :param focus_char: character which should be taken to mark the target
+        :return: list of marked context strings, updated target indices
+        """
+        marked_contexts = []
+        marked_target_inds = []
+
+        if focus_char is not None:
+            for cxt, (tgt_si, tgt_ei) in zip(contexts, target_ses):
+                before_target = cxt[:tgt_si]
+                after_target = cxt[tgt_ei:]
+                marked_cxt = ' '.join([before_target, focus_char, cxt[tgt_si:tgt_ei], focus_char, after_target])
+                assert marked_cxt[tgt_si + 3:tgt_ei + 3] == cxt[tgt_si:tgt_ei]
+                marked_contexts.append(marked_cxt)
+                marked_target_inds.append( (tgt_si+3, tgt_ei+3) )
+        assert len(marked_contexts) == len(marked_target_inds)
+        return marked_contexts, marked_target_inds
