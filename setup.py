99f23e22a5d7d3d1ad026995bfc00f7a3a884021
Anna Breit <anna@breit-mail.at>
2022-02-09 13:51:33 +0100

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='wictsv',
+    version='',
+    packages=['HyperBert', 'model_evaluation'],
+    url='',
+    license='MIT',
+    author='Artem Revenko, Anna Breit',
+    author_email='artreven@gmail.com',
+    description='Word-in-Context Target-Sense-Versification (wic-tsc) dataset and associated models and training scripts'
+)
