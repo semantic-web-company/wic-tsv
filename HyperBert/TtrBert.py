5f87cf2dd0067d6e8aabc475f38c1398de1aabc6
Anna Breit <anna@breit-mail.at>
2022-05-20 15:48:53 +0200

    adding test training script

--- HyperBert/TtrBert.py
+++ HyperBert/TtrBert.py
@@ -1,6 +1,13 @@
+from pathlib import Path
+from typing import Dict
+
+import torch
+from sklearn.metrics import precision_recall_fscore_support
 from torch import nn
 from torch.nn import CrossEntropyLoss
-from transformers import BertPreTrainedModel, BertModel
+from transformers import BertPreTrainedModel, BertModel, AutoTokenizer, EvalPrediction
+
+from model_evaluation.ttr_dataset import TTRDataset
 
 
 class TtrBert():
@@ -55,3 +62,96 @@ class BertForNer(BertPreTrainedModel):
             outputs = (loss,) + outputs
 
         return outputs
+
+
+if __name__ == '__main__':
+
+    import logging
+    import argparse
+    from sklearn.model_selection import train_test_split
+    from transformers import Trainer, TrainingArguments
+    from model_evaluation import data_processors as dp
+
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s %(levelname)-8s %(message)s',)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset_path', nargs='?', default='../data/ttf', type=str)
+    parser.add_argument('--model_output_path', nargs='?', default='./model', type=str)
+    parser.add_argument('--model_name', nargs='?', default='bert-base-uncased', type=str)
+    args = parser.parse_args()
+    base_path = Path(args.dataset_path)
+    output_path = Path(args.model_output_path)
+
+    ttr_path = base_path / 'deutf.train'
+    logging.log(logging.INFO, "Load model")
+
+    model_name = args.model_name
+    tok = AutoTokenizer.from_pretrained(model_name)
+    model = BertForNer.from_pretrained(model_name, num_labels=5)
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model.to(device)
+
+    tokens, nes, unique_nes = dp.read_conll(ttr_path,0, 4)
+
+    train_token, val_token, train_labels, val_labels = train_test_split(tokens, nes, test_size=0.15, random_state = 42)
+    train_token, val_token, train_labels, val_labels = list(train_token), list(val_token), list(train_labels), list(val_labels)
+    def_dict = {"PER" : "eine Person",
+                  "LOC" : "ein Ort",
+                  "ORG": "eine Organisation",
+                  "MISC": "ein Ding"}
+    hyper_dict = {"PER" : ["Person"],
+                  "LOC" : ["Ort"],
+                  "ORG": ["Organisation"],
+                  "MISC": ["Ding"]}
+
+    #print('train', sum([Counter(l) for l in train_labels]))
+    logging.log(logging.INFO, "Load test dataset")
+    train_ds = TTRDataset(train_token,hypernyms=hyper_dict, definitions=def_dict, tokenizer=tok, labels=train_labels)
+
+    #print('test', sum([Counter(l) for l in val_labels]))
+    logging.log(logging.INFO, "Load val dataset")
+    val_ds = TTRDataset(val_token,hypernyms=hyper_dict, definitions=def_dict, tokenizer=tok, labels=val_labels)
+
+
+
+
+    def compute_metrics(p: EvalPrediction) -> Dict:
+        fp = p.predictions
+        binary_preds = (p.predictions > 0).astype(type(p.label_ids[0]))
+        binary = binary_preds.T == p.label_ids
+        acc = binary.mean()
+        precision, r, f1, _ = precision_recall_fscore_support(y_true=p.label_ids, y_pred=binary_preds, average='binary')
+        return {
+            "acc": acc,
+            "F_1": f1,
+            "P": precision,
+            "R": r,
+            "Positive": binary_preds.sum() / binary_preds.shape[0]
+        }
+
+    training_args = TrainingArguments(
+        output_dir=str(output_path),
+        overwrite_output_dir=True,
+        do_train=True,
+        do_eval=True,
+        evaluation_strategy='epoch',
+        num_train_epochs=10,
+        per_device_train_batch_size=4,
+    )
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        # prediction_loss_only=True,
+        train_dataset=train_ds,
+        eval_dataset=val_ds,
+        # eval_dataset=test_ds,
+        compute_metrics=compute_metrics,
+    )
+    logging.log(logging.INFO, "Start training")
+    output = trainer.train()
+    print(f'Training output: {output}')
+    trainer.save_model()
+    #preds = trainer.predict(test_dataset=test_ds)
+    #print(preds)
+    #print(preds.predictions.tolist())
