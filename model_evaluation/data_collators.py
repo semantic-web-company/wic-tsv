15e47d2decdc087a019e132f9e8d70dca6e3e99e
Anna Breit <anna@breit-mail.at>
2022-08-26 15:25:00 +0200

    minor bugfix

--- model_evaluation/data_collators.py
+++ model_evaluation/data_collators.py
@@ -20,27 +20,35 @@ class DataCollatorForTokenClassificationWithOffsetMapping:
     def __call__(self, features):
         label_name = "label" if "label" in features[0].keys() else "labels"
         labels = [feature[label_name].tolist() for feature in features] if label_name in features[0].keys() else None
-        offset_mappings = [feature['offset_mapping'].tolist() for feature in features] if label_name in features[0].keys() else None
+        offset_mappings = [feature['offset_mapping'].tolist() for feature in features] if 'offset_mapping' in features[0].keys() else None
         batch = self.tokenizer.pad(
             features,
             padding=self.padding,
             max_length=self.max_length,
             pad_to_multiple_of=self.pad_to_multiple_of,
             # Conversion to tensors will fail if we have labels as they are not of the same length yet.
-            return_tensors="pt" if labels is None else "np",
+            return_tensors="pt" if labels is None and offset_mappings is None else "np",
         )
 
-        if labels is None:
+        if labels is None and offset_mappings is None:
             return batch
 
         sequence_length = torch.tensor(batch["input_ids"]).shape[1]
         padding_side = self.tokenizer.padding_side
         if padding_side == "right":
-            batch["labels"] = [label + [self.label_pad_token_id] * (sequence_length - len(label)) for label in labels]
-            batch["offset_mapping"] = [offset_mapping + [[0, 0]] * (sequence_length - len(offset_mapping)) for offset_mapping in offset_mappings]
+            if labels is not None:
+                batch["labels"] = [label + [self.label_pad_token_id] * (sequence_length - len(label))
+                                   for label in labels]
+            if offset_mappings is not None:
+                batch["offset_mapping"] = [offset_mapping + [[0, 0]] * (sequence_length - len(offset_mapping))
+                                           for offset_mapping in offset_mappings]
         else:
-            batch["labels"] = [[self.label_pad_token_id] * (sequence_length - len(label)) + label for label in labels]
-            batch["offset_mapping"] = [[[0, 0]] * (sequence_length - len(offset_mapping)) + offset_mapping for offset_mapping in offset_mappings]
+            if labels is not None:
+                batch["labels"] = [[self.label_pad_token_id] * (sequence_length - len(label)) + label
+                                   for label in labels]
+            if offset_mappings is not None:
+                batch["offset_mapping"] = [[[0, 0]] * (sequence_length - len(offset_mapping)) + offset_mapping
+                                           for offset_mapping in offset_mappings]
 
         batch = {k: torch.tensor(v, dtype=torch.int64) for k, v in batch.items() if k != "offset_mapping"}
         return batch
